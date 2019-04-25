c8ae23266c63d4c0d5254e5bbe5e3577fef06fb8
Quickcorp <info@quickcorp.cl>
2019-04-25 17:04:55 -0400

    init

---
+++ spec/qcobjects-syntax-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import QCObjectsSyntaxView from '../lib/qcobjects-syntax-view';
+
+describe('QCObjectsSyntaxView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
